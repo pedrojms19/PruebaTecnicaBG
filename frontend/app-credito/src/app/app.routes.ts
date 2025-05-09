3c2094faa5e65af2dfc8caeafc85b9acd39fac23
Pedro Mendoza <pmendoza_slzr@hotmail.com>
2025-05-09 18:21:01 -0500

    uploading both projects

---
+++ frontend/app-credito/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+
+export const routes: Routes = [{
+    path: 'register',
+    component: RegisterComponent
+},{
+    path: '',
+    component: LoginComponent
+}];
---
+++ frontend/app-credito/src/app/auth/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email = '';
+  password = '';
+  error=""
+}
